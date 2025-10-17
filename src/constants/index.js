c6d1c703ff7eff2ff106f8a57c58ecbcd536ada7
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-17 12:08:14 +0530

    first commit and Navbar component added

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { label: "Store", href: "/store" },
+  { label: "Mac", href: "/mac" },
+  { label: "iPad", href: "/ipad" },
+  { label: "iPhone", href: "/iphone" },
+  { label: "Watch", href: "/watch" },
+  { label: "AirPods", href: "/airpods" },
+  { label: "Support", href: "/support" },
+];
