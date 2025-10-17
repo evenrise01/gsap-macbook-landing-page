a84d14a1cd2b2fb985c484fab10aaea1ef127886
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-17 14:17:35 +0530

    feat: implement productViewer and Zustand for global context management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 const App = () => {
   return (
     <main id="main">
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
