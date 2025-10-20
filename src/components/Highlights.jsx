bbbb2393ce01470e56e4aff1d5e7113a8b3f246c
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-20 10:59:22 +0530

    feat: enhance Highlights component with GSAP animations and responsive design

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Highlights = () => {
-  return (
-    <div>
-      Highlights
-    </div>
-  )
-}
 
-export default Highlights
+  const isMobile = useMediaQuery({query: 'max-width: 1024px'});
+
+  useGSAP(() => {
+    gsap.to(['.left-column', '.right-column'], {
+      scrollTrigger: {
+        trigger: '#highlights',
+        start: isMobile ? 'bottom bottom' : 'top top'
+      },
+      y: 0,
+      stagger: 0.5,
+      opacity: 1,
+      ease: 'power1.inOut',
+      duration: 1
+    })
+  })
+
+  return (
+    <section id="highlights">
+      <h2>There&apos;s never been a better time to upgrade.</h2>
+      <h3>Here&apos;s what you get with the new Macbook Pro.</h3>
+
+      <div className="masonry">
+        <div className="left-column">
+          <div>
+            <img src="/laptop.png" alt="laptop" />
+            <p>Fly through demanding tasks upto 9.8x faster.</p>
+          </div>
+
+          <div>
+            <img src="/sun.png" alt="sun" />
+            <p>
+              A stunning <br /> Liquid Retina XDR <br /> display.
+            </p>
+          </div>
+        </div>
+
+        <div className="right-column">
+          <div className="apple-gradient">
+            <img src="/ai.png" alt="AI" />
+            <p>
+              Built for <br /> <span>Apple intelligence</span>
+            </p>
+          </div>
+
+          <div>
+            <img src="/battery.png" alt="Battery" />
+            <p>
+              Up to
+              <br /> <span className="green-gradient"> 14 more hours </span>
+              battery life.{" "}
+              <span className="text-dark-100"> (Up to 24 hours total.)</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
